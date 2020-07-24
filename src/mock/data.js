01d853a4e758d58ac4711cc45b93d375b3dda01f
rohangupta <rohaninjmu@gmail.com>
2020-07-24 18:55:27 +0530

    Updated Profile

--- src/mock/data.js
+++ src/mock/data.js
@@ -11,16 +11,19 @@ export const headData = {
 export const heroData = {
   title: 'Hello, my name is',
   name: 'Rohan Gupta',
-  subtitle: "I'm a Python Developer",
+  subtitle: "I'm a Python & IOS Developer",
   cta: 'Know more',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: '20190606_111943.jpg',
-  paragraphOne: 'Strong in design and integration with intuitive problem-solving skills. Proficient in PYTHON, DJANGO, HTML, CSS, COMPETITIVE PROGRAMMING, Kivy, Selenium, argparse and many more technical skills in demand today.',
-  paragraphTwo: 'An open source contributor passionate about implementing and launching new projects. Ability to translate business requirements into technical solutions.',
-  paragraphThree: 'Looking to start the career as an entry-level software engineer with a reputed firm driven by technology.',
+  paragraphOne:
+    'Strong in design and integration with intuitive problem-solving skills. Proficient in PYTHON, DJANGO, Swift, SwiftUI, Storyboard, tensorflow and many more technical skills in demand today.',
+  paragraphTwo:
+    'An open source contributor passionate about implementing and launching new projects. Ability to translate business requirements into technical solutions.',
+  paragraphThree:
+    'Looking to start the career as an entry-level software engineer with a reputed firm driven by technology.',
   resume: 'https://drive.google.com/file/d/1ZB3oEi6fG9wGHkQGRxDhP4Tfkm-zmgxz/view?usp=drivesdk', // if no resume, the button will not show up
 };
 
@@ -28,21 +31,21 @@ export const aboutData = {
 export const projectsData = [
   {
     id: uuidv1(),
-    img: 'Screenshot 2020-03-22 at 11.49.57 PM.jpg',
-    title: 'Notes App',
-    info: 'A note taking app made using the django fraamework.',
+    img: 'Covid-19IOS.jpg',
+    title: 'COVID-19 Tracker',
+    info: 'COVID-19 Tracker IOS App 🦠',
     info2: '',
-    url: 'https://rohan-note.herokuapp.com',
-    repo: 'https://github.com/Rohan-cod/notes_app', // if no repo, the button will not show up
+    url: '',
+    repo: 'https://github.com/Rohan-cod/COVID-19_TRACKER', // if no repo, the button will not show up
   },
   {
     id: uuidv1(),
-    img: 'Screenshot 2020-03-22 at 11.53.56 PM.jpg',
-    title: 'Job Postings App',
-    info: 'A web app made using the django framework.',
-    info2: 'Get a list of latest job postings daily.',
-    url: 'https://rohan-jobs.herokuapp.com',
-    repo: 'https://github.com/Rohan-cod/sch_webscr', // if no repo, the button will not show up
+    img: 'Covid-19Django',
+    title: 'COVID19 TRACKER AND VOICE ASSISTANT',
+    info: 'A COVID19 Tracker and Voice Assistant built with Django.',
+    info2: '',
+    url: '',
+    repo: 'https://github.com/Rohan-cod/COVID19_TRACKER_AND_VOICE_ASSISTANT', // if no repo, the button will not show up
   },
   {
     id: uuidv1(),
@@ -58,7 +61,7 @@ export const projectsData = [
 // CONTACT DATA
 export const contactData = {
   cta: 'Want to get in touch? Awesome!',
-  btn: "Let's build together" ,
+  btn: "Let's build together",
   email: 'rohaninjmu@gmail.com',
 };
 
