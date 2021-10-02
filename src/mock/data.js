d982491e38f214dbe498fd21307d18f36e99de0e
Rohan Gupta <rohaninjmu@gmail.com>
2021-10-02 19:42:36 +0530

    Update data.js

--- src/mock/data.js
+++ src/mock/data.js
@@ -24,7 +24,7 @@ export const aboutData = {
     'An open source contributor passionate about implementing and launching new projects. Ability to translate business requirements into technical solutions.',
   paragraphThree:
     'Looking to start the career as an entry-level software engineer with a reputed firm driven by technology.',
-  resume: 'https://drive.google.com/file/d/1OZsVLjQnWCxEODNN8m-dLvMP7lLMB4bt/view?usp=sharing', // if no resume, the button will not show up
+  resume: 'https://drive.google.com/file/d/14OsDzEz0NouPJRoQ7m49sEjAPDr6RNVu/view?usp=sharing', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
@@ -68,16 +68,6 @@ export const contactData = {
 // FOOTER DATA
 export const footerData = {
   networks: [
-    {
-      id: uuidv1(),
-      name: 'whatsapp',
-      url: 'https://api.whatsapp.com/send?phone=+917006482743',
-    },
-    {
-      id: uuidv1(),
-      name: 'phone',
-      url: 'tel:+917006482743',
-    },
     {
       id: uuidv1(),
       name: 'linkedin',
